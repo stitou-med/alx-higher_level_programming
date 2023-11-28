444662f747829bad4dc954bcda07ec6bbb15e3b4
stitou-med <mstitou044@gmail.com>
2023-11-29 00:45:00 +0100

    comb3 update

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,6 @@ for i in range(10):
     for j in range(i, 10):
         if (i == 8) and (j == 9):
             print("{}{}".format(i, j))
+            break
         else:
             print("{}{}".format(i, j), end=", ")
