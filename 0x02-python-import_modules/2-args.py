bc25eab7d20077f133d1a862fbcc6eff3b348314
stitou-med <mstitou044@gmail.com>
2023-11-30 23:31:14 +0100

    0x02. Python - import & modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    n = len(sys.argv)
+    if n > 2:
+        print("{} arguments:".format(n - 1))
+        for i in range(1, n):
+            print("{}: {}".format(i, sys.argv[i]))
+    elif n == 2:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
+    else:
+        print("0 arguments.")
