113995249a5caaf85c23a0268752b6958c4718d1
stitou-med <mstitou044@gmail.com>
2023-11-30 22:43:10 +0100

    0x02. Python - import & modules

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import calculator_1 as cal
     a = 10
-    b = 2
+    b = 5
     print("{} + {} = {}".format(a, b, cal.add(a, b)))
     print("{} - {} = {}".format(a, b, cal.sub(a, b)))
     print("{} * {} = {}".format(a, b, cal.mul(a, b)))
